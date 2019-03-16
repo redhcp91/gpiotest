29000bb19113ec34e07b13adf72c0ee93cc9e33a
redhcp91 <redhcp91@gmail.com>
2019-03-15 21:47:59 -0400

    Eigth Commit

---
+++ User_Input/inputLED.py
@@ -0,0 +1,17 @@
+
+from gpiozero import LED, Button
+from signal import pause
+
+print('Input button number')
+button = input()
+
+print('Input LED1 number')
+led1 = input()
+
+print('Input LED2 number')
+led2 = input()
+
+button.when_pressed = led1.toggle
+button.when_held = led2.toggle
+
+pause()
