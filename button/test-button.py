7aff5027b060f8d5795e9e6146cbeef5668d9c71
redhcp91 <redhcp91@gmail.com>
2019-03-15 18:19:32 -0900

    Ninth Commit

--- button/test-button.py
+++ button/test-button.py
@@ -6,13 +6,16 @@ from signal import pause
 
 led1 = LED(4)
 led2 = LED(17)
-
-button1 = Button(26)
+led3 = LED(27)
+led4 = LED(22)
+button1 = Button(10)
 
 while True:
     button1.wait_for_press()
     led1.toggle()
     led2.toggle()
+    led3.toggle()
+    led4.toggle()
     sleep(0.5)
 
-
+pause()
